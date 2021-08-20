d084ba97ce5fc4cfdcaf803490aef44f2969190d
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-08-20 12:14:21 -0400

    Create archinstaller.sh

---
+++ archinstaller.sh
@@ -0,0 +1,5 @@
+mfs.xfs /dev/sda1
+mount /dev/sda1 /mnt
+pacstrap /mnt base base-devel linux
+genfstab /mnt>/mnt/etc/fstab
+arch-chroot /mnt bash -c 'pacman -Sy grub;grub-install /dev/sda;grub-mkconfig -o /boot/grub/grub.cfg;pacman -Sy vim networkmanager;systemctl enable NetworkManager;passwd';reboot
