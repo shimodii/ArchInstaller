1e97410aa8e5fec8abe3418ae626f12f6e9595bd
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-08-25 03:38:29 +0430

    Create install-gnome.sh

---
+++ install-gnome.sh
@@ -0,0 +1,6 @@
+sudo pacman -S gnome gnome-extra gdm
+clear
+echo "you can active gdm with these steps:"
+echo "1- disable old dm with: sudo systemctl disable <dm>"
+echo "2- enable gdm with: sudo systemctl enable gdm;reboot"
+echo "good luck!"
