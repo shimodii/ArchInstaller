3cc2c7bbee245f986d049050c438596ce9e29762
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-08-25 03:40:40 +0430

    Create install-xfce.sh

---
+++ install-xfce.sh
@@ -0,0 +1,5 @@
+sudo pacman -S xfce4 xfce4-goodies lightdm
+echo "you can active lightdm with these steps:"
+echo "1- disable old dm with: sudo systemctl disable <dm>"
+echo "2- enable lightdm with: sudo systemctl enable lightdm;reboot"
+echo "good luck!"
