179803fa3e4f2ae65572120baab504fe356e2aba
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-08-25 02:30:04 +0430

    Update bios-install.sh

--- bios-install.sh
+++ bios-install.sh
@@ -1,6 +1,73 @@
-mfs.xfs /dev/sda1
-mkswap /dev/sda2
-mount /dev/sda1 /mnt
-pacstrap /mnt base base-devel linux
+########################################
+#                                      #
+#     https://github.com/am-shm        #
+#                                      #
+########################################
+
+############### start ##################
+echo "welcome to arch installer"
+echo "by https://github.com/am-shm"
+printf "enter your root partition: "
+read ROOT
+printf "enter your swap partition: "
+read SWAP
+
+############# installtion ##############
+mfs.xfs $ROOT
+mkswap $SWAP
+mount $ROOT /mnt
+
+############# base system ##############
+pacstrap /mnt base base-devel linux vim vi
+
+########## fstab generation ############
 genfstab /mnt>/mnt/etc/fstab
-arch-chroot /mnt bash -c 'pacman -Sy grub;grub-install /dev/sda;grub-mkconfig -o /boot/grub/grub.cfg;pacman -Sy vim networkmanager;systemctl enable NetworkManager;passwd';reboot
+
+#### chroot & config installed arch ####
+arch-chroot /mnt
+
+#config locale
+mv /etc/locale.gen /etc/locale.gen.old
+touch /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+#config hostname and hosts
+echo "arch" > /etc/hostname
+touch /etc/hosts
+echo "127.0.0.1 	localhost" >> /etc/hosts
+echo "::1 		localhost" >> /etc/hosts
+echo "127.0.1.1 	     arch" >> /etc/hosts
+
+#install and start networkmanager service
+pacman -S networkmanager
+systemctl start NetworkManager
+
+#install and configuration bootloader
+pacman -Sy grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#set root password
+clear
+echo "set root password"
+passwd
+
+#make user
+printf "enter you username: "
+read USERNAME
+useradd -m $USERNAME
+usermod -a -G wheel $USERNAME
+echo "set password for user"
+passwd $USERNAME
+
+#edit sudoers file
+clear
+echo "now uncomment the line include %wheel ALL=(ALL) then :wq"
+printf "[press any key to continue]"
+read KEY
+visudo
+
+#done
+clear
+echo "Done !"
