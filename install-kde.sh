e4cfb438f5281785887f61242a19553fd6bcf77a
Amir Mohammad <53581892+am-shm@users.noreply.github.com>
2021-08-25 03:43:30 +0430

    Create install-kde.sh

---
+++ install-kde.sh
@@ -0,0 +1,5 @@
+sudo pacman -S xorg plasma plasma-wayland-session kde-applications sddm
+echo "you can active sddm with these steps:"
+echo "1- disable old dm with: sudo systemctl disable <dm>"
+echo "2- enable sddm with: sudo systemctl enable sddm;reboot"
+echo "good luck!"
